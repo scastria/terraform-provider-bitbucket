3aaf47ab014763c1d11425d07eee82ac514c42af
Shawn Castrianni <scastrianni@greenstreet.com>
2025-12-18 20:32:33 -0600

    Base framework

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # terraform-provider-bitbucket
+Latest documentation can be found here: https://registry.terraform.io/providers/scastria/bitbucket/latest/docs
---
+++ bitbucket/provider.go
@@ -0,0 +1,27 @@
+package bitbucket
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"collection_path": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+		ResourcesMap:         map[string]*schema.Resource{},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
+	}
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var diags diag.Diagnostics
+	return nil, diags
+}
